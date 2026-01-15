6ad2f2814ce3830c8abd9a67f9675b8835166887
Антон Рязанов <anton@Antons-Mac-mini.local>
2026-01-15 18:02:46 +0700

    add zod

---
+++ src/shared/config/env.ts
@@ -0,0 +1,9 @@
+import "server-only";
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+  DATABASE_URL: z.string().url(),
+});
+
+export const env = envSchema.parse(process.env);
